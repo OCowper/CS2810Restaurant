ce1660f1881b1f857c6ec472dfdfc1164248546e
Oscar Cowper <zkac355@live.rhul.ac.uk>
2023-01-19 22:36:13 +0000

    Added a model (and associated test class) to handle behaviours.

---
+++ src/main/java/restaurant/RestModel.java
@@ -0,0 +1,10 @@
+package restaurant;
+
+/**
+ * Behaviour handler for the system.
+ *
+ * @author zkac355
+ */
+public class RestModel {
+
+}
---
+++ src/test/java/restaurant/ModelControllerTest.java
@@ -0,0 +1,19 @@
+package restaurant;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Testing for the model class.
+ *
+ * @author zkac355
+ */
+public class ModelControllerTest {
+
+  private RestModel testModel;
+
+  @Test // test 1
+  void testClass() {
+    testModel = new RestModel();
+  }
+
+}
