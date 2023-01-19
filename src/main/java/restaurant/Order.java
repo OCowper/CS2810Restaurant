3bad64eb8f6b989bafe36e1d86a4da8e477bda51
Oscar Cowper <zkac355@live.rhul.ac.uk>
2023-01-19 22:57:30 +0000

    created an order class to abstract out primitive obsession smell for handling orders. will be passed between MVC instead of each field seperately handled.

---
+++ src/main/java/restaurant/Order.java
@@ -0,0 +1,14 @@
+package restaurant;
+
+/**
+ * Data storage class to collate all information about an order.
+ *
+ * @author zkac355
+ */
+public class Order {
+  
+  public String[] items;
+  public int tableNum;
+  public float total;
+
+}
---
+++ src/test/java/restaurant/OrderTest.java
@@ -0,0 +1,19 @@
+package restaurant;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Testing for the author class.
+ *
+ * @author zkac355
+ */
+public class OrderTest {
+  
+  private Order testOrder;
+  
+  @Test // test 1
+  void testClass() {
+    testOrder = new Order();
+  }
+
+}
