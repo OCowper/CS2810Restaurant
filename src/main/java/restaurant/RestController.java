5c7fa464379cf2433486045cb71769e62a23b690
Oscar Cowper <zkac355@live.rhul.ac.uk>
2023-01-19 21:40:15 +0000

    as per a test, created a controller class - which will be used to communicate between view and model.

---
+++ src/main/java/restaurant/RestController.java
@@ -0,0 +1,10 @@
+package restaurant;
+
+/**
+ * Controller to facilitate communication between the view (gui) and the model (behaviour).
+ *
+ * @author zkac355
+ */
+public class RestController {
+
+}
---
+++ src/test/java/restaurant/RestControllerTest.java
@@ -0,0 +1,18 @@
+package restaurant;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Test class for the controller class.
+ *
+ * @author zkac355
+ */
+public class RestControllerTest {
+  
+  private RestController testController;
+  
+  @Test // test 1
+  void testClass() {
+    testController = new RestController();
+  } // passed by creating a controller class
+}
