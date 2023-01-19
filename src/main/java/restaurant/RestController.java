1efeb563fbaccd5f4eabca28139d632b12b19119
Oscar Cowper <zkac355@live.rhul.ac.uk>
2023-01-19 21:03:01 +0000

    As motivated by a test, created a controller class. This will handle communication between the views (gui handler) and the model (behaviour handler). This commit also adds all the setup files (gitignore, classpath, checkstyle etc) - let me know if something's not working

---
+++ src/main/java/restaurant/RestController.java
@@ -0,0 +1,10 @@
+package restaurant;
+
+/**
+ * Controller class to facilitate communication between the model and the views.
+ *
+ * @author zkac355
+ */
+public class RestController {
+
+}
---
+++ src/test/java/restaurant/ControllerTest.java
@@ -0,0 +1,20 @@
+package restaurant;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Testing for the controller class.
+ *
+ * @author zkac355
+ */
+public class ControllerTest {
+  
+  private RestController testController;
+  
+  @Test // test 1
+  void classTest() {
+    testController = new RestController();
+  }
+  
+
+}
