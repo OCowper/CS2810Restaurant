beee375b311e68b05b27170f733a2f577e00bc4b
zjac295 <zjac295@live.rhul.ac.uk>
2023-01-24 12:09:14 +0000

    Created Prototype of Customer Menu / Order GUI.I have implemented the following features: a menu for the customer to browse through, a search bar for if the customer would like to search a specific product, an order overview which highlights the customers selection, an entry of the customers table number, the total cost being displayed, a submit order button to send the order to the kitchen.

---
+++ src/main/java/Driver.java
@@ -0,0 +1,25 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Driver extends Application{
+
+  public static void main(String[] args) {
+    launch(args);
+
+  }
+
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+    Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("model.fxml"));
+    Scene scene = new Scene(root, 600, 400); // Creates Scene and Size
+    primaryStage.setScene(scene); //Sets the Primary Stage as Scene
+    primaryStage.setResizable(false); //Makes GUI not resizeable
+    primaryStage.show(); //Displays GUI
+    
+    
+  }
+
+}
---
+++ src/main/java/MainView.java
@@ -0,0 +1,131 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.Separator;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+
+public class MainView {
+
+  @FXML
+  private AnchorPane anchorpane;
+
+  @FXML
+  private Label desserts;
+
+  @FXML
+  private Label drinks;
+
+  @FXML
+  private CheckBox eight;
+
+  @FXML
+  private CheckBox eightteen;
+
+  @FXML
+  private CheckBox eleven;
+
+  @FXML
+  private CheckBox fifteen;
+
+  @FXML
+  private CheckBox five;
+
+  @FXML
+  private CheckBox four;
+
+  @FXML
+  private CheckBox fourteen;
+
+  @FXML
+  private Label mains;
+
+  @FXML
+  private CheckBox nine;
+
+  @FXML
+  private CheckBox nineteen;
+
+  @FXML
+  private CheckBox one;
+
+  @FXML
+  private ScrollPane scrollpane;
+
+  @FXML
+  private TextField searchbar;
+
+  @FXML
+  private ListView<?> searchlist;
+
+  @FXML
+  private Label selectionlbl;
+
+  @FXML
+  private Separator seperator;
+
+  @FXML
+  private CheckBox seven;
+
+  @FXML
+  private CheckBox seventeen;
+
+  @FXML
+  private CheckBox six;
+
+  @FXML
+  private CheckBox sixteen;
+
+  @FXML
+  private Label starters;
+
+  @FXML
+  private Button submitbtn;
+
+  @FXML
+  private Label tablelbl;
+
+  @FXML
+  private TextField tablenotxt;
+
+  @FXML
+  private CheckBox ten;
+
+  @FXML
+  private CheckBox thirteen;
+
+  @FXML
+  private CheckBox three;
+
+  @FXML
+  private Label titlelbl;
+
+  @FXML
+  private Label totallbl;
+
+  @FXML
+  private TextField totaltxt;
+
+  @FXML
+  private CheckBox twelve;
+
+  @FXML
+  private CheckBox twenty;
+
+  @FXML
+  private CheckBox two;
+
+  @FXML
+  private TextField userselections;
+
+  @FXML
+  private VBox vbox;
+}
