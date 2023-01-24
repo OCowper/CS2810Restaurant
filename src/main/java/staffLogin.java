12abe50cb67039cb2986e16364b54cb194d2bc0d
zjac295 <zjac295@live.rhul.ac.uk>
2023-01-24 20:29:21 +0000

    Implemented a Staff Login Prototype, It asks for the staff members ID and Password, also has buttons for login/return to menu/forgotten password with no functionality at the moment.

---
+++ src/main/java/staffLogin.java
@@ -0,0 +1,32 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class staffLogin {
+
+    @FXML
+    private Button forgotBtn;
+
+    @FXML
+    private Label idLbl;
+
+    @FXML
+    private TextField idTxt;
+
+    @FXML
+    private Button loginBtn;
+
+    @FXML
+    private Label passLbl;
+
+    @FXML
+    private TextField passwordTxt;
+
+    @FXML
+    private Button returnBtn;
+
+    @FXML
+    private Label titleLbl;
+
+}
