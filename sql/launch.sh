7cd7fa2d87dddd6635c1aa7e6118390bcc20d438
DoingGitStuff <78906194+DoingGitStuff@users.noreply.github.com>
2023-01-24 15:14:58 +0000

    added additional comments to about running postgres via docker

--- sql/launch.sh
+++ sql/launch.sh
@@ -2,6 +2,8 @@
 # it can be installed at https://www.docker.com/
 # to initialise run the file as is.
 docker run --volume=PROJECT-pg:/var/lib/postgresql/data --name=PROJECTDB -p 5432:5432 -e POSTGRES_PASSWORD=yaydatayaybase -d postgres:15
+# if this fails you may need to download the postgres image. run the below to download it
+# docker pull postgres:15
 # to stop run:
 # docker stop PROJECTDB 
 # to start it up again run:
