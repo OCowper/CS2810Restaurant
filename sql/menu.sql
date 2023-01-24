b956f4d41d334e791d978c84c4c3dfb8b96b7cf5
DoingGitStuff <78906194+DoingGitStuff@users.noreply.github.com>
2023-01-24 14:47:22 +0000

    changed menu_items.id column data type to serial and removed auto_increment attribute

--- sql/menu.sql
+++ sql/menu.sql
@@ -1,5 +1,5 @@
-CREATE TABLE menu_items (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+CREATE IF NOT EXISTS TABLE menu_items (
+    id SERIAL PRIMARY KEY,
     dish TEXT NOT NULL,
     description TEXT,
     price MONEY NOT NULL
