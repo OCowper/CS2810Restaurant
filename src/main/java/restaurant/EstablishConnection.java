c5cd2cd2b03883a9b313327cd84c1c7ee62e60b5
Oscar Cowper <zkac355@live.rhul.ac.uk>
2023-01-25 12:20:56 +0000

    Added a connection class with a static method for connecting to the postgre database. This may need to be edited in future when we are clear on where the database is stored; for now it connects to the teaching server

---
+++ src/main/java/restaurant/EstablishConnection.java
@@ -0,0 +1,55 @@
+package restaurant;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+/**
+ * Class containing methods to establish a database connection.
+ *
+ * @author zkac355
+ */
+public class EstablishConnection {
+
+  /**
+   * Establishes a connection to the database based on user id and password. Static.
+   */
+  public static void establishConnection() {
+
+    Scanner userInput = new Scanner(System.in);
+    System.out.println("Enter your username: ");
+    String user = userInput.nextLine();
+    System.out.println("Enter your password: ");
+    String password = userInput.nextLine();
+    String database = "teachdb.cs.rhul.ac.uk";
+
+    userInput.close();
+
+    System.out.println("Attempting Connection");
+    Connection connection = null;
+    try {
+      String protocol = "jdbc:postgresql://";
+      String databaseName = "/CS2855/";
+      String fullUrl = protocol + database + databaseName + user;
+      connection = DriverManager.getConnection(fullUrl, user, password);
+    } catch (SQLException e) {
+      String errorMsg = e.getMessage();
+      if (errorMsg.contains("authentication failed")) {
+        System.out.println("ERROR: Incorrect password");
+      } else {
+        System.out.println("Connection failure");
+        e.printStackTrace();
+      }
+    }
+    if (connection != null) {
+      System.out.println("connection successful");
+    } else {
+      System.out.println("connection failed");
+      System.exit(1);
+    }
+
+  }
+
+
+}
