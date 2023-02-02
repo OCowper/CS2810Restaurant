098bbee759d83bb47be214d39b80c1c7e56126d9
zjac295 <zjac295@live.rhul.ac.uk>
2023-02-02 21:14:42 +0000

    Implemented a return to menu button on the StaffPasswordReset Page, added method to return to the Main Menu.

--- src/main/java/StaffPasswordReset.java
+++ src/main/java/StaffPasswordReset.java
@@ -1,9 +1,15 @@
+import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 public class StaffPasswordReset {
 
@@ -25,11 +31,17 @@ public class StaffPasswordReset {
   @FXML
   private Button returnbtn;
 
-    @FXML
-    void isPressed(ActionEvent event) {
+  @FXML
+  void isPressed(ActionEvent event) {
 
-    }
-    
-    
+  }
+
+  public void handleForgotPassReturn(ActionEvent event) throws IOException {
+    Parent startViewParent = FXMLLoader.load(getClass().getResource("FoodMenuView.fxml"));
+    Scene startView = new Scene(startViewParent);
+    Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+    window.setScene(startView);
+    window.show();
+  }
 
 }
