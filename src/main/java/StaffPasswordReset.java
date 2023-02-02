c1476c1bd739b652c503de55b5bdc6120cbef0ff
zhac080 <zhac080@cim-ts-node-02.cc.rhul.local>
2023-02-02 00:01:55 +0000

    Imported the sample code from scenebuilder for Staff Password Reset Page

--- src/main/java/StaffPasswordReset.java
+++ src/main/java/StaffPasswordReset.java
@@ -1,6 +1,30 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
 
-public class StaffPasswordReset {
-	
-	
+public class PleaseProvideControllerClassName {
+
+    @FXML
+    private Pane Background;
+
+    @FXML
+    private TextField CustomerID;
+
+    @FXML
+    private TextField Email;
+
+    @FXML
+    private Text PromptText;
+
+    @FXML
+    private Button ResetLink;
+
+    @FXML
+    void isPressed(ActionEvent event) {
+
+    }
 
 }
