bc8541073c82095543d1e0f0328fe18dfe68561b
zhac080 <zhac080@cim-ts-node-02.cc.rhul.local>
2023-02-02 01:51:01 +0000

    Imported code skeleton from scenebuilder for Kitchen Screen display.

--- src/main/java/KitchenScreen.java
+++ src/main/java/KitchenScreen.java
@@ -1,4 +1,29 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
 
 public class KitchenScreen {
+
+    @FXML
+    private Pane backgroundDisplay;
+
+    @FXML
+    private Text inProgressOrders;
+
+    @FXML
+    private ListView<?> inProgressOrdersList;
+
+    @FXML
+    private Text newOrders;
+
+    @FXML
+    private ListView<?> newOrdersList;
+
+    @FXML
+    private Text orderHistory;
+
+    @FXML
+    private ListView<?> orderHistoryList;
 
 }
