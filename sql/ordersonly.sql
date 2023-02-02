9ce7f689b130951dcee9c8c694cf7f4e1a95f619
Wood James (2021) ZKAC256 <james.wood.2021@live.rhul.ac.uk>
2023-02-02 14:12:41 +0000

    Adding waiterID to the OrderTable, foreign key of StaffID

--- sql/ordersonly.sql
+++ sql/ordersonly.sql
@@ -4,4 +4,6 @@ CREATE TABLE IF NOT EXISTS Orders(
     table_Num varchar(15),
     price numeric(1000, 1),
     confirm boolean, 
+    waiter_id varchar(15),	 
+    foreign key (waiter_id) references Staff(staff_id),
     primary key (order_Num));
