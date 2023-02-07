bf788a982b5cb98ee1e128176c9a59c528db3b9e
Oscar Cowper <zkac355@live.rhul.ac.uk>
2023-02-07 23:18:08 +0000

    Added a login class and test file for waiter logins. will try to write this relatively independantly of the gui so i can test it nicely, as i want to get back into writing proper junit tests

---
+++ src/main/java/restaurant/LoginSubmit.java
@@ -0,0 +1,10 @@
+package restaurant;
+
+/**
+ * Class supporting submission of staff logins to the system.
+ *
+ * @author zkac355
+ */
+public class LoginSubmit {
+  
+}
---
+++ src/test/java/restaurant/LoginSubmitTest.java
@@ -0,0 +1,20 @@
+package restaurant;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Test for the LoginSubmit class.
+ *
+ * @author zkac355
+ */
+public class LoginSubmitTest {
+  
+  
+  LoginSubmit testLogin;
+  
+  @Test // test 1
+  void testClass() {
+    testLogin = new LoginSubmit();
+  }
+
+}
