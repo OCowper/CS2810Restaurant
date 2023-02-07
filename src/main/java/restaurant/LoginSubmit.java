b792ffb3fa54eadcd3887df980dfb7a5034f09d3
Oscar Cowper <zkac355@live.rhul.ac.uk>
2023-02-07 23:36:03 +0000

    Added a test case to compare submitted usrn and pass to stored ones. Currently faked.

--- src/main/java/restaurant/LoginSubmit.java
+++ src/main/java/restaurant/LoginSubmit.java
@@ -1,10 +1,16 @@
 package restaurant;
 
+import java.sql.Connection;
+
 /**
  * Class supporting submission of staff logins to the system.
  *
  * @author zkac355
  */
 public class LoginSubmit {
+  
+  public static Boolean submitLogin(Connection connection, String staffId, String password) {
+    return staffId == "12345" && password == "pass1";
+  }
   
 }
--- src/test/java/restaurant/LoginSubmitTest.java
+++ src/test/java/restaurant/LoginSubmitTest.java
@@ -1,5 +1,9 @@
 package restaurant;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.sql.Connection;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -11,10 +15,28 @@ public class LoginSubmitTest {
   
   
   LoginSubmit testLogin;
+  static Connection connection;
+  Boolean flag;
+  
+  @BeforeAll
+  static void connectionSetup() {
+    connection = EstablishConnection.establishConnection();
+  }
+  
+  @BeforeEach
+  void setup() {
+    testLogin = new LoginSubmit();
+  }
   
   @Test // test 1
   void testClass() {
     testLogin = new LoginSubmit();
+  } // test for class creation
+  
+  @Test // test 2
+  void testMatch() {
+    flag = LoginSubmit.submitLogin(connection, "12345", "pass1");
+    assertEquals(flag, true, "flag should be true");
   }
 
 }
