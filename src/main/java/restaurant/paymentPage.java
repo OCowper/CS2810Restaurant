2c640595886b0c0779bab3a29b3bd616bd36c592
zhac080 <zhac080@cim-ts-node-01.cc.rhul.local>
2023-02-21 23:13:18 +0000

    Acquired javafx code for the payment page from scenebuilder.

--- src/main/java/restaurant/paymentPage.java
+++ src/main/java/restaurant/paymentPage.java
@@ -1,5 +1,52 @@
 package restaurant;
 
+import import javafx.scene.control.TextField;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+
 public class paymentPage {
+
+    @FXML
+    private Text CVV;
+
+    @FXML
+    private AnchorPane bgAnchorpane;
+
+    @FXML
+    private VBox bgVbox;
+
+    @FXML
+    private Text cardNumber;
+
+    @FXML
+    private TextField cardNumberField;
+
+    @FXML
+    private TextField cvvField;
+
+    @FXML
+    private Text expiryDate;
+
+    @FXML
+    private TextField expiryDateField;
+
+    @FXML
+    private ImageView logo;
+
+    @FXML
+    private Text nameOnCard;
+
+    @FXML
+    private TextField nameOnCardField;
+
+    @FXML
+    private Button payNowButton;
+
+    @FXML
+    private Text paymentHeading;
 
 }
