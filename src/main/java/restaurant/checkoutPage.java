bad4490f86f0857d58c67996b6c28af643768586
zhac080 <zhac080@cim-ts-node-01.cc.rhul.local>
2023-02-21 22:35:57 +0000

    Acquired the fxml code from scenebuilder for displaying customer checkout view.

--- src/main/java/restaurant/checkoutPage.java
+++ src/main/java/restaurant/checkoutPage.java
@@ -1,5 +1,68 @@
 package restaurant;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+
 public class checkoutPage {
+
+    @FXML
+    private Button aboutUsButton;
+
+    @FXML
+    private AnchorPane bgAnchorPane;
+
+    @FXML
+    private ImageView bgImage;
+
+    @FXML
+    private Button cartButton;
+
+    @FXML
+    private Text heading;
+
+    @FXML
+    private Text itemsHeading;
+
+    @FXML
+    private ListView<?> itemsListview;
+
+    @FXML
+    private ImageView logoImage;
+
+    @FXML
+    private Button menuButton;
+
+    @FXML
+    private Button placeOrderButton;
+
+    @FXML
+    private Text priceHeading;
+
+    @FXML
+    private ListView<?> priceListview;
+
+    @FXML
+    private Text quantityHeading;
+
+    @FXML
+    private ListView<?> quantityListview;
+
+    @FXML
+    private Button removeItemButton;
+
+    @FXML
+    private TextField totalAmount;
+
+    @FXML
+    private Text totalField;
+
+    @FXML
+    private VBox virtualBox;
 
 }
