9eddf1ebff289591f36cb40523524b1b2d81284e
zhac080 <zhac080@cim-ts-node-01.cc.rhul.local>
2023-02-22 22:33:04 +0000

    Acquired the code from the scenebuilder for corresponding javafx file.

--- src/main/java/restaurant/paymentConfirmation.java
+++ src/main/java/restaurant/paymentConfirmation.java
@@ -1,5 +1,30 @@
 package restaurant;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+
 public class paymentConfirmation {
+
+    @FXML
+    private AnchorPane bgAnchorpane;
+
+    @FXML
+    private ImageView logoImage;
+
+    @FXML
+    private TextField orderNumber;
+
+    @FXML
+    private Text orderNumberHeading;
+
+    @FXML
+    private Text thankingCustomer;
+
+    @FXML
+    private VBox vBox;
 
 }
