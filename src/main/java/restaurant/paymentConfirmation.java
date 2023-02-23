cac4f8ff8ca573944a1f0506240a512c01d48f0a
Mathushan Shanthan <zjac295@cim-ts-node-03.cc.rhul.local>
2023-02-23 07:48:59 +0000

    Updated .FXML and .Java files for Payment Confirmation.

--- src/main/java/restaurant/paymentConfirmation.java
+++ src/main/java/restaurant/paymentConfirmation.java
@@ -1,30 +1,69 @@
 package restaurant;
 
+import java.io.IOException;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
-public class paymentConfirmation {
+public class paymentConfirmation implements Subject, ViewInterface {
 
-    @FXML
-    private AnchorPane bgAnchorpane;
+  @FXML
+  private AnchorPane bgAnchorpane;
 
-    @FXML
-    private ImageView logoImage;
+  @FXML
+  private VBox bgVbox;
 
-    @FXML
-    private TextField orderNumber;
+  @FXML
+  private Button emptyButton;
 
-    @FXML
-    private Text orderNumberHeading;
+  @FXML
+  private ImageView oaxacaImageView;
 
-    @FXML
-    private Text thankingCustomer;
+  @FXML
+  private Label orderLabel;
 
-    @FXML
-    private VBox vBox;
+  @FXML
+  private Label ordernumberLabel;
+
+  @FXML
+  private Label orderplacedLabel;
+
+  @FXML
+  private Text paymentHeading;
+
+  @FXML
+  private Button returnButton;
+  
+  public Observer obs;
+
+  @Override
+  public void addObservers(Observer obs) {
+    this.obs = obs;
+
+  }
+
+  @Override
+  public void notifyObservers(Observer obs) {}
+
+  @Override
+  public void acceptBoolean(Boolean bool) {}
+
+  @Override
+  public void startup() {
+    // TODO Auto-generated method stub
+    
+  }
+
 
 }
