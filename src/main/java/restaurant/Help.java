844b0a72234fa9d973daec1224971f83b228c19d
Mathushan Shanthan <zjac295@cim-ts-node-01.cc.rhul.local>
2023-02-28 21:42:27 +0000

    Created java Class and linked to Help.fxml file that has been created.

---
+++ src/main/java/restaurant/Help.java
@@ -0,0 +1,61 @@
+package restaurant;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+
+public class Help {
+
+    @FXML
+    private Button aboutUsButton;
+
+    @FXML
+    private AnchorPane bgAnchorPane;
+
+    @FXML
+    private ImageView bgImage;
+
+    @FXML
+    private Button cartButton;
+
+    @FXML
+    private Button helpBtn;
+
+    @FXML
+    private Text helpTitle;
+
+    @FXML
+    private Button menuButton;
+
+    @FXML
+    private ImageView oaxacaImageView;
+
+    @FXML
+    private VBox virtualBox;
+
+    @FXML
+    void handleCartBtn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleCustomerBtn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleHelpBtn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleMenuReturn(MouseEvent event) {
+
+    }
+
+}
