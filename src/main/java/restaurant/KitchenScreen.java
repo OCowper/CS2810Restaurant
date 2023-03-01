97cf3491211013c000329d34b8396f675a059e5a
Mathushan Shanthan <zjac295@cim-ts-node-03.cc.rhul.local>
2023-03-01 15:14:42 +0000

    Added Notification option to KitchenScreen navigation bar. Linked traversal between notification screen and kitchen screen.

--- src/main/java/restaurant/KitchenScreen.java
+++ src/main/java/restaurant/KitchenScreen.java
@@ -174,6 +174,19 @@ public class KitchenScreen implements Subject, ViewInterface {
     window.setScene(startView);
     window.show();
   }
+  
+  @FXML
+  public void handleNotificationsBtn(ActionEvent event) throws IOException {
+    FXMLLoader loader =
+        new FXMLLoader(getClass().getClassLoader().getResource("NotificationScreen.fxml"));
+    Parent startViewParent = loader.load();
+    Scene startView = new Scene(startViewParent);
+    Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+    obs.setView(loader.getController());
+    obs.orderStartup();
+    window.setScene(startView);
+    window.show();
+  }
 
   @FXML
   private void listExit() {
