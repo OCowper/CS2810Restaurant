03dc277c6cb066fbf75b33cdf06e495c5b0fd9dd
Mathushan Shanthan <zjac295@cim-ts-node-02.cc.rhul.local>
2023-03-07 21:15:17 +0000

    Updated AboutPage Controller Class, Added Methods to swap scenes from AboutPage to others on Navigation bar.

--- src/main/java/restaurant/AboutPage.java
+++ src/main/java/restaurant/AboutPage.java
@@ -1,14 +1,21 @@
 package restaurant;
 
+import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
-public class AboutPage {
+public class AboutPage implements Subject, ViewInterface {
 
     @FXML
     private Text aboutUsHeading;
@@ -35,25 +42,99 @@ public class AboutPage {
     private VBox sidebarVBox;
 
     @FXML
-    void handleCartBtn(ActionEvent event) {
+    void handleAboutBtn(ActionEvent event) throws IOException {
+      FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("AboutPage.fxml"));
+      Parent staffLodinParent = loader.load();
+      Scene staffLogin = new Scene(staffLodinParent);
+
+      Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+      obs.setView(loader.getController());
+
+      window.setScene(staffLogin);
+      window.show();
+    }
+
+    /**
+     * Changes the scene from initial one to food menu view.
+     *
+     * @param event "customerbtn" button pressed
+     * @throws IOException when there is a problem with loading the fxml file
+     */
+    public void handleCustomerBtn(ActionEvent event) throws IOException {
+      FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("CustomerMenu.fxml"));
+      Parent foodMenuparent = loader.load();
+
+      Scene foodMenu = new Scene(foodMenuparent);
+      Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+      obs.setView(loader.getController());
+      obs.orderStartup();
+      window.setScene(foodMenu);
+      window.show();
 
     }
 
-    @FXML
-    void handleCustomerBtn(ActionEvent event) {
+    public void handleMenuReturn(MouseEvent event) throws IOException {
+      FXMLLoader loader =
+          new FXMLLoader(getClass().getClassLoader().getResource("newLandingPage.fxml"));
+      Parent paymentParent = loader.load();
+      Scene payment = new Scene(paymentParent);
+
+      Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+      obs.setView(loader.getController());
+
+      window.setScene(payment);
+      window.show();
+    }
+
+
+    public void handleHelpBtn(ActionEvent event) throws IOException {
+      FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("Help.fxml"));
+      Parent staffLodinParent = loader.load();
+      Scene staffLogin = new Scene(staffLodinParent);
+
+      Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+      obs.setView(loader.getController());
+
+      window.setScene(staffLogin);
+      window.show();
+    }
+
+    /**
+     * Handling for if the cart screen switcher is pressed.
+     *
+     * @param event representing the button press
+     * @throws IOException if an IO error occurs.
+     */
+    public void handleCartBtn(ActionEvent event) throws IOException {
+      FXMLLoader loader =
+          new FXMLLoader(getClass().getClassLoader().getResource("checkoutPage.fxml"));
+      Parent cartParent = loader.load();
+      Scene checkout = new Scene(cartParent);
+
+      Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+      obs.setView(loader.getController());
+
+      window.setScene(checkout);
+      window.show();
+    }
+    
+    public Observer obs;
+
+    @Override
+    public void addObservers(Observer obs) {
+      this.obs = obs;
 
     }
 
-    @FXML
-    void handleHelpBtn(ActionEvent event) {
+    @Override
+    public void notifyObservers(Observer obs) {}
+
+    @Override
+    public void acceptBoolean(Boolean bool) {}
+
+    @Override
+    public void startup() {
+      // TODO Auto-generated method stub
 
     }
-
-    @FXML
-    void handleReturnBtn(ActionEvent event) {
-
-    }
-
 }
-
-
