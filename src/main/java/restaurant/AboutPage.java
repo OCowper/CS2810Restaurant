ce09900bcee3c23a9adf708307a0b96b104274e1
zhac080 <zhac080@cim-ts-node-03.cc.rhul.local>
2023-03-07 14:52:24 +0000

    Acquired code from scenebuilder to display "about us" page to the customer.

--- src/main/java/restaurant/AboutPage.java
+++ src/main/java/restaurant/AboutPage.java
@@ -1,5 +1,59 @@
 package restaurant;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+
 public class AboutPage {
 
+    @FXML
+    private Text aboutUsHeading;
+
+    @FXML
+    private AnchorPane bgPane;
+
+    @FXML
+    private Button cartButton;
+
+    @FXML
+    private Button helpButton;
+
+    @FXML
+    private ImageView logoImage;
+
+    @FXML
+    private Button menuButton;
+
+    @FXML
+    private Button returnButton;
+
+    @FXML
+    private VBox sidebarVBox;
+
+    @FXML
+    void handleCartBtn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleCustomerBtn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleHelpBtn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleReturnBtn(ActionEvent event) {
+
+    }
+
 }
+
+
