d52a1663d330a46ddbf4fa90bfca263a7cc7b4bc
Mathushan Shanthan <zjac295@cim-ts-node-03.cc.rhul.local>
2023-03-08 16:04:01 +0000

    Updated Java file to include methods to swap onto other pages on navigation var.

--- src/main/java/restaurant/CustomerNotificationScreen.java
+++ src/main/java/restaurant/CustomerNotificationScreen.java
@@ -1,7 +1,12 @@
 package restaurant;
 
+import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
@@ -11,8 +16,9 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
-public class CustomerNotificationScreen {
+public class CustomerNotificationScreen  implements Subject, ViewInterface {
 
     @FXML
     private Label OrderIDLabel;
@@ -65,34 +71,110 @@ public class CustomerNotificationScreen {
     @FXML
     private VBox virtualBox;
 
-    @FXML
-    void handleAboutBtn(ActionEvent event) {
+    /**
+     * Changes the scene from initial one to food menu view.
+     *
+     * @param event "customerbtn" button pressed
+     * @throws IOException when there is a problem with loading the fxml file
+     */
+    public void handleCustomerBtn(ActionEvent event) throws IOException {
+      FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("CustomerMenu.fxml"));
+      Parent foodMenuparent = loader.load();
+
+      Scene foodMenu = new Scene(foodMenuparent);
+      Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+      obs.setView(loader.getController());
+      obs.orderStartup();
+      window.setScene(foodMenu);
+      window.show();
 
     }
 
+    public void handleMenuReturn(MouseEvent event) throws IOException {
+      FXMLLoader loader =
+          new FXMLLoader(getClass().getClassLoader().getResource("newLandingPage.fxml"));
+      Parent paymentParent = loader.load();
+      Scene payment = new Scene(paymentParent);
+
+      Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+      obs.setView(loader.getController());
+
+      window.setScene(payment);
+      window.show();
+    }
+
+
+    public void handleHelpBtn(ActionEvent event) throws IOException {
+      FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("Help.fxml"));
+      Parent staffLodinParent = loader.load();
+      Scene staffLogin = new Scene(staffLodinParent);
+
+      Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+      obs.setView(loader.getController());
+      obs.orderStartup();
+      window.setScene(staffLogin);
+      window.show();
+    }
+
+    /**
+     * Handling for if the cart screen switcher is pressed.
+     *
+     * @param event representing the button press
+     * @throws IOException if an IO error occurs.
+     */
+    public void handleCartBtn(ActionEvent event) throws IOException {
+      FXMLLoader loader =
+          new FXMLLoader(getClass().getClassLoader().getResource("checkoutPage.fxml"));
+      Parent cartParent = loader.load();
+      Scene checkout = new Scene(cartParent);
+
+      Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+      obs.setView(loader.getController());
+
+      window.setScene(checkout);
+      window.show();
+    }
+    
     @FXML
-    void handleCartBtn(ActionEvent event) {
+    void handleAboutBtn(ActionEvent event) throws IOException {
+      FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("AboutPage.fxml"));
+      Parent staffLodinParent = loader.load();
+      Scene staffLogin = new Scene(staffLodinParent);
+      Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+      obs.setView(loader.getController());
+      window.setScene(staffLogin);
+      window.show();
+    }
+    
+    void handleTrackBtn(ActionEvent event) throws IOException {
+      FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("CustomerNotificationScreen.fxml"));
+      Parent staffLodinParent = loader.load();
+      Scene staffLogin = new Scene(staffLodinParent);
+      Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+      obs.setView(loader.getController());
+      window.setScene(staffLogin);
+      window.show();
+    }
+
+    public Observer obs;
+
+    @Override
+    public void addObservers(Observer obs) {
+      this.obs = obs;
 
     }
 
-    @FXML
-    void handleCustomerBtn(ActionEvent event) {
+    @Override
+    public void notifyObservers(Observer obs) {}
+
+    @Override
+    public void acceptBoolean(Boolean bool) {}
+
+    @Override
+    public void startup() {
+      // TODO Auto-generated method stub
 
     }
 
-    @FXML
-    void handleHelpBtn(ActionEvent event) {
-
-    }
-
-    @FXML
-    void handleMenuReturn(MouseEvent event) {
-
-    }
-
-    @FXML
-    void handleTrackBtn(ActionEvent event) {
-
-    }
 
 }
