f349137952847abb868d5db3c3df338647ae5cad
Mathushan Shanthan <zjac295@cim-ts-node-03.cc.rhul.local>
2023-03-08 16:00:57 +0000

    Updated Navigation Bar on customer side to Include Track Order.

--- src/main/java/restaurant/CustomerNotificationScreen.java
+++ src/main/java/restaurant/CustomerNotificationScreen.java
@@ -1,5 +1,98 @@
 package restaurant;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+
 public class CustomerNotificationScreen {
+
+    @FXML
+    private Label OrderIDLabel;
+
+    @FXML
+    private ListView<?> OrderIDListView;
+
+    @FXML
+    private TextField OrderIDTextField;
+
+    @FXML
+    private Label OrderIdLabel;
+
+    @FXML
+    private Label StatusLabel;
+
+    @FXML
+    private ListView<?> StatusListView;
+
+    @FXML
+    private Button TrackBtn;
+
+    @FXML
+    private Button TrackOrderButton;
+
+    @FXML
+    private Button aboutUsButton;
+
+    @FXML
+    private AnchorPane bgAnchorPane;
+
+    @FXML
+    private ImageView bgImage;
+
+    @FXML
+    private Button cartButton;
+
+    @FXML
+    private Button helpBtn;
+
+    @FXML
+    private Text helpTitle;
+
+    @FXML
+    private Button menuButton;
+
+    @FXML
+    private ImageView oaxacaImageView;
+
+    @FXML
+    private VBox virtualBox;
+
+    @FXML
+    void handleAboutBtn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleCartBtn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleCustomerBtn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleHelpBtn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleMenuReturn(MouseEvent event) {
+
+    }
+
+    @FXML
+    void handleTrackBtn(ActionEvent event) {
+
+    }
 
 }
