3429d8eb237c1e4203222b4de323e512b802fa2e
IreneeG <iren.gubacheva@gmail.com>
2023-03-09 12:15:28 +0000

    Merged with the updated db.

--- sql/ResturantTables.sql
+++ sql/ResturantTables.sql
@@ -1,31 +1,13 @@
-
 CREATE TABLE IF NOT EXISTS Staff(
     staff_ID varchar(15) NOT NULL,
-    password varchar(15) NOT NULL,
-    first_Name varchar(15) NOT NULL,
-    last_Name varchar(15) NOT NULL, 
-    job_Title varchar(15) NOT NULL,
-    email varchar(15) NOT NULL,
-    phone_Number varchar(15),
-    address varchar(15),
+    password varchar(60) NOT NULL,
+    first_name varchar(20) NOT NULL,
+    last_name varchar(20) NOT NULL, 
+    job_title varchar(15) NOT NULL, -- waiter, kitchen staff, admin, etc.
+    email varchar(30),
+    phone varchar(15),
+    home_address varchar(15),
     primary key (staff_ID)
-);
-
-
-CREATE TABLE IF NOT EXISTS Bookings(
-    booking_Num varchar(15),
-    guest_Amount numeric(30, 1),
-    host_Email varchar(15),
-    booking_Time varchar(15),
-    booking_Date varchar(15)
-    primary key (booking_Num)
-);
-
-CREATE TABLE IF NOT EXISTS Covers(
-    booking_Num varchar(15),
-    cover_Date varchar(15),
-    cover_Amount numeric(1000, 1),
-    foreign key (booking_Num) references Bookings(booking_Num)
 );
 
 CREATE TABLE IF NOT EXISTS Items(
@@ -39,4 +21,23 @@ CREATE TABLE IF NOT EXISTS Items(
     available boolean NOT NULL, 
     primary key (item_id)
 );
+
+CREATE TABLE IF NOT EXISTS Orders(
+    order_num numeric(6, 0) NOT NULL,
+    order_description varchar(100),
+    table_num numeric(2, 0) NOT NULL,
+    price numeric(9, 2) NOT NULL,
+    order_status status,
+    waiter_id varchar(15),	 ---- do we need to associate waiter with an order ?????
+    foreign key (waiter_id) references Staff(staff_id),
+    primary key (order_num)
+);
+CREATE TYPE status AS ENUM ('recieved', 'confirmed', 'being prepared', 'ready', 'received', 'settled'); --combine ready and received?
+
+-- do we need to keep done orders separately?
+CREATE TABLE IF NOT EXISTS DoneOrders(
+    order_num numeric(6, 0),
+    cancelled boolean,
+    foreign key (order_num) references Orders(order_num),
+    primary key (order_num));
 
