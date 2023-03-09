b8fb5206eaad24fece5a8d29b3029cf64a7d26e7
Oscar Cowper <zkac355@live.rhul.ac.uk>
2023-03-09 12:49:50 +0000

    more merge conflicts fixed

--- sql/tempDbInserts.sql
+++ sql/tempDbInserts.sql
@@ -3,18 +3,9 @@ INSERT INTO Items VALUES ('31', 'Tea', 2.15, 'Premium English brakfast with a sp
 INSERT INTO Items VALUES ('32', 'Fries', 4.5, 'Triple battered thick potato fries.', 'potatoes, batter: [flour, milk, eggs]', 794, 'Starters', TRUE);
 INSERT INTO Items VALUES ('33', 'Hallumi', 5.7, 'Out speciality hallumi sticks. The choice of any wise human.', 'hallumi cheese:[milk]', 856, 'Starters', TRUE);
 INSERT INTO Items VALUES ('34', 'Caprese', 8, 'The freshest Italian mozarella cut into pieces on a plate with pesto, tomatoes and balsamico.', 'mozarella cheese, tomatoes, italian pesto: [basil, olive oil, pine nuts, garlic], balsamico sauce', 650, 'Starters', TRUE);
-<<<<<<< HEAD
 INSERT INTO Items VALUES ('35', 'Steak', 25, 'No description needed. Steak. The best of it. Comes with fries and spring salad.', 'beef, butter', 1340, 'Mains', TRUE);
 INSERT INTO Items VALUES ('36', 'Pasta', 15, 'Ask our waiter for today`s choice of pasta dishes. They all cost the same and all taste like heaven.', 'ask waiter for clarification', 1400, 'Mains', TRUE);
 INSERT INTO Items VALUES ('37', 'Risotto', 12.8, 'Authentic gourmet mashroom risotto with rosemary, black pepper and truffle salt.', 'rice, mashrooms, heavy cream, beef stock, white wine, parsley', 947, 'Mains', TRUE);
 INSERT INTO Items VALUES ('38', 'Paella', 17.6, 'Some would argue we it`s just a seafood risotto; we say they are nothing alike. Our spanish paella features all your favourite crustacean.', 'rice, king shrimps, mussles, cod, basa fillet, crab meat, white wine, bell peppers, cherry tomatoes', 1100, 'Mains', TRUE);
 insert into Staff values('100575405', 'pass1', 'Oscar', 'Cowper', 'Waiter', 'oscar.cowper', '977856', 'new road');
 insert into Orders values('0', 'voidorder', '12', 25.67, 'recieved', '100575405');
-=======
-INSERT INTO Items VALUES ('35', 'Steak', 25, 'No description needed. Steak. The best of it. Comes with fries and spring salad.', 'beef, butter', 1340,'Mains', TRUE);
-INSERT INTO Items VALUES ('36', 'Pasta', 15, 'Ask our waiter for today`s choice of pasta dishes. They all cost the same and all taste like heaven.', 'ask waiter for clarification', 1400, 'Mains', TRUE);
-INSERT INTO Items VALUES ('37', 'Risotto', 12.8, 'Authentic gourmet mashroom risotto with rosemary, black pepper and truffle salt.', 'rice, mashrooms, heavy cream, beef stock, white wine, parsley', 947, 'Mains', TRUE);
-INSERT INTO Items VALUES ('38', 'Paella', 17.6, 'Some would argue we it`s just a seafood risotto; we say they are nothing alike. Our spanish paella features all your favourite crustacean.', 'rice, king shrimps, mussles, cod, basa fillet, crab meat, white wine, bell peppers, cherry tomatoes', 1100, 'Mains', TRUE);
-insert into staff values('100575405', 'pass1', 'Oscar', 'Cowper', 'Waiter', 'oscar.cowper', '977856', 'new road');
-insert into orders values('0', 'voidorder', '0', '0.0', 't', '100575405');
->>>>>>> origin/menuDescrUpdate
