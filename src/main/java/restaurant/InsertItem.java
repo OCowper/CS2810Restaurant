1005034a0595ea9ed4d8a814e463dac59a59f755
Vokshi <ZJAC311@live.rhul.ac.uk>
2023-03-22 17:47:41 +0000

    Updated the code to have getters and setters

--- src/main/java/restaurant/InsertItem.java
+++ src/main/java/restaurant/InsertItem.java
@@ -1,7 +1,106 @@
 package restaurant;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+
+class Item {
+    private String itemId;
+    private String itemName;
+    private double price;
+    private String itemDescription;
+    private String ingredients;
+    private int calories;
+    private String itemCategory;
+    private boolean available;
+    private String imagePath;
+
+    public Item(String itemId, String itemName, double price, String itemDescription, String ingredients, int calories, String itemCategory, boolean available, String imagePath) {
+        this.itemId = itemId;
+        this.itemName = itemName;
+        this.price = price;
+        this.itemDescription = itemDescription;
+        this.ingredients = ingredients;
+        this.calories = calories;
+        this.itemCategory = itemCategory;
+        this.available = available;
+        this.imagePath = imagePath;
+    }
+
+    // Getters and setters
+
+    public String getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(String itemId) {
+        this.itemId = itemId;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getItemDescription() {
+        return itemDescription;
+    }
+
+    public void setItemDescription(String itemDescription) {
+        this.itemDescription = itemDescription;
+    }
+
+    public String getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(String ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public int getCalories() {
+        return calories;
+    }
+
+    public void setCalories(int calories) {
+        this.calories = calories;
+    }
+
+    public String getItemCategory() {
+        return itemCategory;
+    }
+
+    public void setItemCategory(String itemCategory) {
+        this.itemCategory = itemCategory;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+}
 
 public class InsertItem {
 
